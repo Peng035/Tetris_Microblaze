0f7f330c3faf4550eda82713dfaa9cee521ab2b2
Peng035 <489272751@qq.com>
2024-03-05 21:06:23 +0100

    add  silent gap
    no volume control
    music is friends

--- music_player.vhd
+++ music_player.vhd
@@ -61,19 +61,22 @@ type state_type is (idle,
                       );
 type note_table is array (integer range <>) of integer;
 
-constant  T_M : integer := 10000;
+--constant  T_M : integer := 10000;
 -- tempo in bpm                     
-constant TEMPO  : integer := 180;
--- number of 10000 (T_M) clk cycles for 1/4 beat
-constant QUATER_BEAT : integer := integer(150000/TEMPO);
+constant TEMPO  : integer := 120;
+-- number of clk cycles for 1/4 beat
+constant QUATER_BEAT : integer := integer(60*25e6/TEMPO);
+-- silence cycles after each note
+constant SILENT_GAP:     integer := 6e6;
 -- number of notes in the music segment
-constant TOTAL_NOTE_NUM : integer := 46;
+constant TOTAL_NOTE_NUM : integer := 42;
                       
 signal my_note_table : note_table(0 to TOTAL_NOTE_NUM-1);
 signal my_len_table  : note_table(0 to TOTAL_NOTE_NUM-1);
 
 signal tone_index    : integer range 0 to 31 := 15;
 signal volume       : integer range 0 to 10  :=10;
+signal m_Key        : integer range 0 to 10 := 8;
 
 signal current_state : state_type;
 signal next_state : state_type;
@@ -83,11 +86,11 @@ signal tone_index_buff: integer range 0 to 31 :=0;
 -- 1 means quater beat, 2 means half beat, 4 means 1 beat.
 signal  tone_len_buff:  integer range 0 to 8  := 4;  
 -- the note index to loop within the music segment 46 notes in total
-signal note_index : integer range 0 to  TOTAL_NOTE_NUM-1 :=0;
+signal note_Index : integer range 0 to  TOTAL_NOTE_NUM-1 :=0;
 -- counter for note length control
 signal beat_cnt : integer range 0 to  8*QUATER_BEAT -1 :=0;
 -- multiplier counter 
-signal m_cnt : integer range 0 to T_M -1 := 0;
+--signal m_cnt : integer range 0 to T_M -1 := 0;
 -- flag to fectch next note
 signal next_note: std_logic;
 
@@ -118,99 +121,119 @@ PORT MAP (
 	probe3 => note_len_prob
 );       
         
-  my_note_table(0) <=   19; 
-  my_note_table(1) <=   16;
-  my_note_table(2) <=   17;
+  my_note_table(0) <=   0; 
+  my_note_table(1) <=   19;
+  my_note_table(2) <=   19;
   my_note_table(3) <=   18;
-  my_note_table(4) <=   19;
-  my_note_table(5) <=   18;
-  my_note_table(6) <=   17;
-  my_note_table(7) <=   16;
-  my_note_table(8) <=   15;
-  my_note_table(9) <=   15;
-  my_note_table(10) <=  17;      
-  my_note_table(11) <=  19;
-  my_note_table(12) <=  18;
-  my_note_table(13) <=  17;
-  my_note_table(14) <=  16;
-  my_note_table(15) <=  16;      
-  my_note_table(16) <=  16;
-  my_note_table(17) <=  17;
-  my_note_table(18) <=  18;
-  my_note_table(19) <=  19;
-  my_note_table(20) <=  17;  
-  my_note_table(21) <=  15;
-  my_note_table(22) <=  15;
-  my_note_table(23) <=  0;
-  my_note_table(24) <=  18;
-  my_note_table(25) <=  18; 
-  my_note_table(26) <=  20;
-  my_note_table(27) <=  22;
-  my_note_table(28) <=  21;
-  my_note_table(29) <=  20;
-  my_note_table(30) <=  19; 
-  my_note_table(31) <=  19;
-  my_note_table(32) <=  17;
-  my_note_table(33) <=  19;
-  my_note_table(34) <=  18;
-  my_note_table(35) <=  17; 
-  my_note_table(36) <=  16;
-  my_note_table(37) <=  16;
-  my_note_table(38) <=  16;
-  my_note_table(39) <=  17;
-  my_note_table(40) <=  18;    
-  my_note_table(41) <=  19;
-  my_note_table(42) <=  17;
-  my_note_table(43) <=  15;
-  my_note_table(44) <=  15;
-  my_note_table(45) <=  0;
+  my_note_table(4) <=   17;
+  my_note_table(5) <=   15;
+  
+  my_note_table(6) <=   15;
+  my_note_table(7) <=   15;
+  my_note_table(8) <=   17;
+  my_note_table(9) <=   18;
+  my_note_table(10) <=  17;
+        
+  my_note_table(11) <=  0; 
+  my_note_table(12) <=  19;
+  my_note_table(13) <=  19;
+  my_note_table(14) <=  18;
+  my_note_table(15) <=  17;      
+  my_note_table(16) <=  15;
+  
+  my_note_table(17) <=  15;
+  my_note_table(18) <=  15;  
+  my_note_table(19) <=  17;
+  my_note_table(20) <=  18;  
+  my_note_table(21) <=  17;
+  
+  my_note_table(22) <=  0;
+  my_note_table(23) <=  7;
+  my_note_table(24) <=  10;
+  my_note_table(25) <=  10; 
+  my_note_table(26) <=  11;
+  my_note_table(27) <=  13;
+  
+  my_note_table(28) <=  13;
+  my_note_table(29) <=  12;
+  my_note_table(30) <=  11; 
+  my_note_table(31) <=  11;
+  my_note_table(32) <=  10;
+  
+  my_note_table(33) <=  10;
+  my_note_table(34) <=  7;
+  my_note_table(35) <=  10; 
+  my_note_table(36) <=  11;
+  my_note_table(37) <=  11;
+  
+  my_note_table(38) <=  11;
+  my_note_table(39) <=  0;
+  my_note_table(40) <=  0;    
+  my_note_table(41) <=  0;
+  
+--  my_note_table(42) <=  12;
+--  my_note_table(43) <=  15;
+--  my_note_table(44) <=  14;
+--  my_note_table(45) <=  12;
+--  my_note_table(46) <=  11;
+--  my_note_table(47) <=  10;
   
   my_len_table(0) <=    4  ;
-  my_len_table(1) <=    2  ;
+  my_len_table(1) <=    4  ;
   my_len_table(2) <=    2  ;
   my_len_table(3) <=    2  ;
-  my_len_table(4) <=    1  ;
-  my_len_table(5) <=    1  ;
+  my_len_table(4) <=    2  ;
+  my_len_table(5) <=    2  ;
+  
   my_len_table(6) <=    2  ;
-  my_len_table(7) <=    2  ;
-  my_len_table(8) <=    4  ;
-  my_len_table(9) <=    2  ;
-  my_len_table(10) <=   2  ;
+  my_len_table(7) <=    4  ;
+  my_len_table(8) <=    2  ;
+  my_len_table(9) <=    4  ;
+  my_len_table(10) <=   4  ;
+  
   my_len_table(11) <=   4  ;
-  my_len_table(12) <=   2  ;
+  my_len_table(12) <=   4  ;
   my_len_table(13) <=   2  ;
-  my_len_table(14) <=   4  ;
-  my_len_table(15) <=   1  ;
-  my_len_table(16) <=   1  ;
+  my_len_table(14) <=   2  ;
+  my_len_table(15) <=   2  ;
+  my_len_table(16) <=   2  ;
+  
   my_len_table(17) <=   2  ;
   my_len_table(18) <=   4  ;
-  my_len_table(19) <=   4  ;
+  my_len_table(19) <=   2  ;
   my_len_table(20) <=   4  ;
   my_len_table(21) <=   4  ;
+  
   my_len_table(22) <=   4  ;
-  my_len_table(23) <=   4  ;
-  my_len_table(24) <=   4  ;
+  my_len_table(23) <=   2  ;
+  my_len_table(24) <=   2  ;
   my_len_table(25) <=   2  ;
-  my_len_table(26) <=   2  ;
-  my_len_table(27) <=   4  ;
-  my_len_table(28) <=   2  ;
+  my_len_table(26) <=   4  ;
+  my_len_table(27) <=   2  ;
+  
+  my_len_table(28) <=   4  ;
   my_len_table(29) <=   2  ;
-  my_len_table(30) <=   4  ;
+  my_len_table(30) <=   2  ;
   my_len_table(31) <=   2  ;
-  my_len_table(32) <=   2  ;
-  my_len_table(33) <=   4  ;
-  my_len_table(34) <=   2  ;
-  my_len_table(35) <=   2  ;
+  my_len_table(32) <=   6  ;
+  
+  my_len_table(33) <=   2  ;
+  my_len_table(34) <=   4  ;
+  my_len_table(35) <=   4  ;
   my_len_table(36) <=   4  ;
-  my_len_table(37) <=   1  ;
-  my_len_table(38) <=   1  ;
-  my_len_table(39) <=   2  ;
+  my_len_table(37) <=   2  ;
+  
+  my_len_table(38) <=   2  ;
+  my_len_table(39) <=   4  ;
   my_len_table(40) <=   4  ;
   my_len_table(41) <=   4  ;
-  my_len_table(42) <=   4  ;
-  my_len_table(43) <=   4  ;
-  my_len_table(44) <=   4  ;
-  my_len_table(45) <=   4  ;
+  
+--  my_len_table(42) <=   1  ;
+--  my_len_table(43) <=   2  ;
+--  my_len_table(44) <=   2  ;
+--  my_len_table(45) <=   2  ;
+--  my_len_table(46) <=   1  ;
+--  my_len_table(47) <=   1  ;
   
   -- debug probe
   tone_prob <= std_logic_vector(TO_UNSIGNED(tone_index_buff, tone_prob'length));
@@ -266,15 +289,19 @@ PORT MAP (
             case current_state is
               when idle =>             
                 next_note <= '0';         -- clear the flag
-                note_index <= 0;          -- reset the index
+                note_Index <= 0;          -- reset the index
                 tone_index_buff <= 0;     -- reset the tone index
                 tone_len_buff <= 0;       -- reset the tone length
                 
               when fetch_note =>
                 -- fetch the note and the length from the lookup table
                 -- accoring to the note index
-                tone_index_buff <= my_note_table(note_index);
-                tone_len_buff <= my_len_table(note_index);   
+                if my_note_table(note_Index) = 0 then
+                    tone_index_buff <= 0;       -- 0 means silence so don't bias with key
+                else
+                    tone_index_buff <= my_note_table(note_Index) + m_Key;
+                end if;
+                tone_len_buff <= my_len_table(note_Index);   
                       
               when play_note =>
                 -- update the beat cnt to control the tone length
@@ -282,19 +309,20 @@ PORT MAP (
                     beat_cnt <= beat_cnt + 1;
                 else
                     beat_cnt <= 0;
-                    if m_cnt < T_M -1  then
-                        m_cnt <= m_cnt +1; 
-                    else 
-                        m_cnt <= 0;
-                        -- set the flag to indicate moving to next note
-                        next_note <= '1'; 
-                        -- increase the note index
-                        if note_index < TOTAL_NOTE_NUM -1 then
-                            note_index <= note_index + 1 ;
-                        else
-                            note_index <= 0;
-                        end if;
+                    
+                    -- set the flag to indicate moving to next note
+                    next_note <= '1'; 
+                    -- increase the note index
+                    if note_Index < TOTAL_NOTE_NUM -1 then
+                        note_Index <= note_Index + 1 ;
+                    else
+                        note_Index <= 0;
                     end if;
+                    
+                end if;
+                -- make the silence gap at the end of each note
+                if beat_cnt > tone_len_buff * QUATER_BEAT - SILENT_GAP then
+                    tone_index_buff <= 0;
                 end if;
                         
             end case;
